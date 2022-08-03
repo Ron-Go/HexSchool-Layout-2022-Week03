7252d9f09b8bf77123e80d0ae95311d8b08afe5d
Ron <hpleain@gmail.com>
2022-08-03 16:37:39 +0800

    Update 2022-08-03T08:37:35.118Z

--- app/assets/js/all.js
+++
@@ -1,5 +0,0 @@
-let hamburger = document.querySelector('.hamburger');
-
-hamburger .addEventListener('click', () => {
-  document.querySelector('.nav').classList.toggle('nav--active');
-});
---
+++ assets/js/all.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', function () {
+  document.querySelector('.nav').classList.toggle('nav--active');
+});
+//# sourceMappingURL=all.js.map
